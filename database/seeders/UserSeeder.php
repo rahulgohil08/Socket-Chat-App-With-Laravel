0c9acff1e4ca9c736febb233dd4d6c479185e777
rahulgohil08 <rg703612@gmail.com>
2021-07-20 17:38:38 +0530

    Faker has dummy entry added.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,6 +10,22 @@ class UserSeeder extends Seeder
 
     public function run()
     {
+
+
+        User::factory()->count(1)->create([
+            'name' => "Jane Doe",
+            'email' => "jane@gmail.com",
+            'mobile_no' => "0123456789",
+            'password' => 'password', // password
+        ]);
+
+        User::factory()->count(1)->create([
+            'name' => "John Doe",
+            'email' => "john@gmail.com",
+            'mobile_no' => "1234567890",
+            'password' => 'password', // password
+        ]);
+
         User::factory()->count(5)->create();
     }
 }
